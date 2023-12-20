5dfa2c047227ddc863c9cc37d66276251a2a1385
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2023-12-21 02:24:53 +0530

    Add files via upload

---
+++ WPI_streamlit.py
@@ -0,0 +1,42 @@
+import os
+import requests
+import streamlit as st
+
+def download_file(url, filename):
+    try:
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+
+        st.success(f"File downloaded successfully as {filename}")
+        return True
+
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error: {e}")
+        return False
+
+def create_download_link(filename, label='Download File'):
+    """Create a download link to the file."""
+    download_link = f'<a href="/downloads/{filename}" download="{filename}" target="_blank">{label}</a>'
+    return download_link
+
+def main():
+    st.title("File Downloader App")
+
+    download_url = 'https://eaindustry.nic.in/indx_download_1112/monthly_index_202311.xls'
+    file_name = 'monthly_index_file.xls'
+
+    if st.button("Download File"):
+        if download_file(download_url, file_name):
+            st.write("File processing completed.")
+            
+            app_path = os.path.dirname(os.path.abspath(__file__))
+
+            file_path = os.path.join(app_path, file_name)
+
+            st.markdown(create_download_link(file_name), unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
