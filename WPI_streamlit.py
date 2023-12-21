8467e1ba18d49fe20127e8f1577c422c6d7a7444
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2023-12-21 10:49:47 +0530

    Update WPI_streamlit.py

--- WPI_streamlit.py
+++ WPI_streamlit.py
@@ -1,5 +1,5 @@
-import os
 import requests
+import pandas as pd
 import streamlit as st
 
 def download_file(url, filename):
@@ -10,33 +10,47 @@ def download_file(url, filename):
         with open(filename, 'wb') as file:
             file.write(response.content)
 
-        st.success(f"File Fetched successfully as {filename}")
+        st.success(f"File processed successfully as {filename}")
         return True
 
     except requests.exceptions.RequestException as e:
         st.error(f"Error: {e}")
         return False
 
-def create_download_link(filename, label='Download File'):
-    """Create a download link to the file."""
-    download_link = f'<a href="/downloads/{filename}" download="{filename}" target="_blank">{label}</a>'
-    return download_link
+def process_file(filename):
+    try:
+        # Read Excel file into a pandas DataFrame
+        df = pd.read_excel(filename)
+
+        # Display the first few rows of the DataFrame
+        st.write("First few rows of the DataFrame:")
+        st.write(df.head())
+
+        # Perform additional data processing or analysis as needed
+
+        return True
+
+    except Exception as e:
+        st.error(f"Error processing file: {e}")
+        return False
 
 def main():
     st.title("WPI File Downloader App")
 
+    # Hardcoded download URL and file name
     download_url = 'https://eaindustry.nic.in/indx_download_1112/monthly_index_202311.xls'
     file_name = 'monthly_index_file.xls'
 
-    if st.button("Fetch File"):
+    # Download button
+    if st.button("Process File"):
         if download_file(download_url, file_name):
-            st.write("File processing completed.")
-            
-            app_path = os.path.dirname(os.path.abspath(__file__))
-
-            file_path = os.path.join(app_path, file_name)
-
-            st.markdown(create_download_link(file_name), unsafe_allow_html=True)
+            st.write("File processing started.")
+            if process_file(file_name):
+                st.success("File processing completed.")
+            else:
+                st.error("Failed to process the file.")
+        else:
+            st.error("Failed to download the file.")
 
 if __name__ == "__main__":
     main()
