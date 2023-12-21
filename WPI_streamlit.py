1f3943c05f51b14e83795e68d8d8d60915f30530
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2023-12-21 11:48:53 +0530

    Update WPI_streamlit.py

--- WPI_streamlit.py
+++ WPI_streamlit.py
@@ -11,11 +11,15 @@ def download_file(url, filename):
         with open(filename, 'wb') as file:
             file.write(response.content)
 
-        st.success(f"File Processed successfully as {filename}")
+        st.success(f"File downloaded successfully as {filename}")
         return True
 
+    except requests.exceptions.HTTPError as e:
+        st.error(f"HTTP error ({e.response.status_code}): {e.response.reason}")
+        return False
+
     except requests.exceptions.RequestException as e:
-        st.error(f"Error: {e}")
+        st.error(f"Error downloading file: {e}")
         return False
 
 def process_file(filename):
@@ -36,19 +40,28 @@ def main():
     download_url = 'https://eaindustry.nic.in/indx_download_1112/monthly_index_202311.xls'
     file_name = 'monthly-index-file.xls'
 
-    if st.button("Process File"):
-        if download_file(download_url, file_name):
-            st.write("File processing started.")
-            if process_file(file_name):
-                st.success("File processing completed.")
-                
-                file_path = os.path.relpath(file_name)
-                
-                st.markdown(f'<a href="{file_path}" download="{file_name}">Download File - {file_name}</a>', unsafe_allow_html=True)
+    try:
+        if st.button("Download and Process File"):
+            if download_file(download_url, file_name):
+                st.write("File processing started.")
+                if process_file(file_name):
+                    st.success("File processing completed.")
+                    
+                    # Display a download button
+                    with open(file_name, 'rb') as file:
+                        file_content = file.read()
+                    st.download_button(
+                        label=f'Download File - {file_name}', 
+                        key='download_button',
+                        data=file_content,
+                        file_name=file_name
+                    )
+                else:
+                    st.error("Failed to process the file.")
             else:
-                st.error("Failed to process the file.")
-        else:
-            st.error("Failed to download the file.")
+                st.error("Failed to download the file. The file may not be available on the site.")
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")
 
 if __name__ == "__main__":
     main()
